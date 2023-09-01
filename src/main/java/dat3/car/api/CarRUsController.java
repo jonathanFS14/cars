5bc26dd45d4deab6491e3e780d27a54635aa35bb
jonathanFS14 <jona08p8@stud.kea.dk>
2023-09-01 15:01:26 +0200

    ...

---
+++ src/main/java/dat3/car/api/CarRUsController.java
@@ -0,0 +1,9 @@
+package dat3.car.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class CarRUsController {
+}
